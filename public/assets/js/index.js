0a93499ee15fcbee2f8f7f84a60f94bc13e06b2c
Brad Cas <bmcasalvieri@gmail.com>
2019-05-02 00:17:06 -0400

    on click for note is almost working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -14,8 +14,9 @@ function printNote(noteInfo) {
     .addClass("card mb-2")
 
   const $noteTitle = $("<div>")
-    .addClass("card-header")
+    .addClass("note card-header")
     .text(noteInfo.title)
+    .attr("data-id", noteInfo.id)
     .appendTo($card);
 
   $notes.append($card);
@@ -24,6 +25,7 @@ function printNote(noteInfo) {
 $(document).ready(runNotesQuery);
 
 $(document).ready(function () {
+  // Event listener for submitting note/click add note button
   $("#note-form").on("submit", function (event) {
     event.preventDefault();
     const newNote = {
@@ -47,5 +49,39 @@ $(document).ready(function () {
     $("#note-body").val("");
     $("#notes").empty();
     runNotesQuery();
-  })
-})
+  });
+
+  // Event listener for clicking delete note button
+  $("#delete-btn").on("click", function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/api/notes",
+      method: "DELETE"
+    }).then(function () {
+      $("#note-title").val("");
+      $("#note-body").val("");
+      $("#notes").empty();
+      unNotesQuery();
+    });
+  });
+
+});
+
+// Event listener for clicking on saved note
+$(document).on("click", ".note", function () {
+  // Grab the id # for the note that was clicked on
+  const noteId = $(this).attr("data-id");
+
+  // Do an AJAX call to get all notes. Loop through each note to see if id # is the same as one clicked. If so, set values of form fields equal to text in note
+  $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  }).then(function (notesData) {
+    notesData.forEach(note => {
+      if (note.id === noteId) {
+        $("#note-title").val(note.title);
+        $("#note-body").val(note.body);
+      };
+    });
+  });
+});
