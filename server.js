d573e7308a0ff2a7dcdc2b6b6d919e9a5b9f3906
Brad Cas <bmcasalvieri@gmail.com>
2019-05-01 20:31:30 -0400

    set up middleware files

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const connection = require("./db/connection");
 const app = express();
 const PORT = 3000;
 
-// Configure the express app to accept JSON from the client
+// Configure the express app to handle data parsing
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+app.use(express.static("public"));
 
+// Sets up route middleware
+// Use the apiRoutes file for any apiRoutes
+// Use the htmlRoutes file for all other routes
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 // Start the app on port 3000 and log a message
 app.listen(PORT, function () {
