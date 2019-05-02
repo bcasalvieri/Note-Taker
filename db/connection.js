626081aeaff6c040415d6f1476a80f0e4a2465e5
Brad Cas <bmcasalvieri@gmail.com>
2019-05-01 23:33:11 -0400

    db password

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.password,
+    password: "Pa$$w0rd",
     database: "note_takerDB"
   });
 }
