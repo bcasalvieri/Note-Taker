f88b9f84cdff4148084fb675013d25f5eb6cf0cf
Brad Cas <bmcasalvieri@gmail.com>
2019-05-01 23:35:23 -0400

    password in .env file

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Pa$$w0rd",
+    password: process.env.password,
     database: "note_takerDB"
   });
 }
